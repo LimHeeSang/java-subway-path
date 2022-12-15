fde8bd9b7e722f0e143153fd809b67296ca1a87b
limheesang <dlagmltkd12@naver.com>
2022-12-15 15:13:07 +0900

    feat: 최소 시간 기준용 그래프 만들기

---
+++ src/test/java/subway/domain/model/ShortestTimePathTest.java
@@ -0,0 +1,18 @@
+package subway.domain.model;
+
+import org.junit.jupiter.api.Test;
+import subway.domain.repository.SectionRepository;
+import subway.domain.repository.StationRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShortestTimePathTest {
+
+    @Test
+    void 최단시간_계산용_그래프_테스트() {
+        ShortestTimePath path = new ShortestTimePath(StationRepository.stations(), SectionRepository.sections());
+        PathResult pathResult = path.calculatePath(new Station("교대역"), new Station("양재역"));
+        assertThat(pathResult.getWeight()).isEqualTo(10);
+        assertThat(pathResult.getStations()).containsExactly(new Station("교대역"), new Station("남부터미널역"), new Station("양재역"));
+    }
+}
