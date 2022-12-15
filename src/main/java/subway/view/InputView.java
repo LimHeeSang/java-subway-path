1e2abcc06dd6e8705004d30662e31d24debac42e
limheesang <dlagmltkd12@naver.com>
2022-12-15 16:40:42 +0900

    feat: 출발역, 도착역 입력 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -8,6 +8,8 @@ import java.util.function.Supplier;
 public class InputView {
 
     private static final String INPUT_FEATURE_MESSAGE = "## 원하는 기능을 선택하세요.";
+    private static final String START_STATION_MESSAGE = "## 출발역을 입력하세요.";
+    private static final String END_STATION_MESSAGE = "## 도착역을 입력하세요.";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -22,6 +24,16 @@ public class InputView {
     public PathStandard inputPathStandard() {
         System.out.println(INPUT_FEATURE_MESSAGE);
         return PathStandard.from(scanner.nextLine());
+    }
+
+    public String inputStartStation() {
+        System.out.println(START_STATION_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public String inputEndStation() {
+        System.out.println(END_STATION_MESSAGE);
+        return scanner.nextLine();
     }
 
     public <T> T input(Supplier<T> supplier) {
